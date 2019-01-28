1ae25a89f38c954c2cc76bcbfcc0d870def65c54
nishantdania <nishantdania@gmail.com>
2019-01-28 21:26:28 +0800

    Prettier gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,86 +1,84 @@
-const _ = require(`lodash`)
-const Promise = require(`bluebird`)
-const path = require(`path`)
-const config = require(`./src/utils/siteConfig`)
-const { paginate } = require(`gatsby-awesome-pagination`)
+const _ = require(`lodash`);
+const Promise = require(`bluebird`);
+const path = require(`path`);
+const config = require(`./src/utils/siteConfig`);
+const { paginate } = require(`gatsby-awesome-pagination`);
 
 /**
-* Here is the place where Gatsby creates the URLs for all the
-* posts, tags, pages and authors that we fetched from the Ghost site.
-*/
+ * Here is the place where Gatsby creates the URLs for all the
+ * posts, tags, pages and authors that we fetched from the Ghost site.
+ */
 exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
+  const { createPage } = actions;
 
-    /**
-    * Posts
-    */
-    const createPosts = new Promise((resolve, reject) => {
-        const postTemplate = path.resolve(`./src/templates/post.js`)
-        const indexTemplate = path.resolve(`./src/templates/index.js`)
-        resolve(
-            graphql(`
-                {
-                    allGhostPost(
-                        sort: {order: ASC, fields: published_at},
-                        filter: {
-                            slug: {ne: "data-schema"}
-                        }
-                    ) {
-                        edges {
-                            node {
-                                slug
-                            }
-                        }
-                    }
-                }`
-            ).then((result) => {
-                if (result.errors) {
-                    return reject(result.errors)
-                }
+  /**
+   * Posts
+   */
+  const createPosts = new Promise((resolve, reject) => {
+    const postTemplate = path.resolve(`./src/templates/post.js`);
+    const indexTemplate = path.resolve(`./src/templates/index.js`);
+    resolve(
+      graphql(`
+        {
+          allGhostPost(
+            sort: { order: ASC, fields: published_at }
+            filter: { slug: { ne: "data-schema" } }
+          ) {
+            edges {
+              node {
+                slug
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          return reject(result.errors);
+        }
 
-                if (!result.data.allGhostPost) {
-                    return resolve()
-                }
+        if (!result.data.allGhostPost) {
+          return resolve();
+        }
 
-                const items = result.data.allGhostPost.edges
+        const items = result.data.allGhostPost.edges;
 
-                _.forEach(items, ({ node }) => {
-                    // This part here defines, that our posts will use
-                    // a `/:slug/` permalink.
-                    node.url = `/${node.slug}/`
+        _.forEach(items, ({ node }) => {
+          // This part here defines, that our posts will use
+          // a `/:slug/` permalink.
+          node.url = `/${node.slug}/`;
 
-                    createPage({
-                        path: node.url,
-                        component: path.resolve(postTemplate),
-                        context: {
-                            // Data passed to context is available
-                            // in page queries as GraphQL variables.
-                            slug: node.slug,
-                        },
-                    })
-                })
+          createPage({
+            path: node.url,
+            component: path.resolve(postTemplate),
+            context: {
+              // Data passed to context is available
+              // in page queries as GraphQL variables.
+              slug: node.slug,
+            },
+          });
+        });
 
-                // Pagination for posts, e.g., /, /page/2, /page/3
-                paginate({
-                    createPage,
-                    items: items,
-                    itemsPerPage: config.postsPerPage,
-                    component: indexTemplate,
-                    pathPrefix: ({ pageNumber }) => {
-                        if (pageNumber === 0) {
-                            return `/`
-                        } else {
-                            return `/page`
-                        }
-                    },
-                })
+        // Pagination for posts, e.g., /, /page/2, /page/3
+        paginate({
+          createPage,
+          items: items,
+          itemsPerPage: config.postsPerPage,
+          component: indexTemplate,
+          pathPrefix: ({ pageNumber }) => {
+            if (pageNumber === 0) {
+              return `/`;
+            } else {
+              return `/page`;
+            }
+          },
+        });
 
-                return resolve()
-            })
-        )
-    })
+        return resolve();
+      })
+    );
+  });
 
-    /**
+  /**
     * Tags
     const createTags = new Promise((resolve, reject) => {
         const tagsTemplate = path.resolve(`./src/templates/tag.js`)
@@ -155,7 +153,7 @@ exports.createPages = ({ graphql, actions }) => {
     })
     */
 
-    /**
+  /**
     * Authors
     const createAuthors = new Promise((resolve, reject) => {
         const authorTemplate = path.resolve(`./src/templates/author.js`)
@@ -229,7 +227,7 @@ exports.createPages = ({ graphql, actions }) => {
     })
     */
 
-    /**
+  /**
     * Pages
     const createPages = new Promise((resolve, reject) => {
         const pageTemplate = path.resolve(`./src/templates/page.js`)
@@ -283,7 +281,7 @@ exports.createPages = ({ graphql, actions }) => {
     })
     */
 
-    // Please read the details in the README of this repository to use the tags, authors and pages templates 
-    // return Promise.all([createPosts, createTags, createAuthors, createPages])
-    return Promise.all([createPosts])
-}
+  // Please read the details in the README of this repository to use the tags, authors and pages templates
+  // return Promise.all([createPosts, createTags, createAuthors, createPages])
+  return Promise.all([createPosts]);
+};
