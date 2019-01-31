b20138c8456ed9a6739eeb967aeab2c4d4befe5a
nishantdania <nishantdania@gmail.com>
2019-01-31 21:07:32 +0800

    Added whoami

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,16 +3,34 @@ import styled from "styled-components"
 
 import Hamburger from '../components/hamburger'
 
+const Container = styled.div`
+  max-width: 850px;
+  margin: auto;
+`
+
 const Nav = styled.div`
-  padding: 20px;
+  padding: 1rem;
+`
+const WhoAmI = styled.div`
+  font-size: 3.2rem;
+  line-height: 4rem;
+  font-family: -apple-system-headline, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+  font-weight: 800; 
+  max-width: 640px;
+  padding: 0 1rem;
 `
 
 const HomePage = () => (
-  <>
+  <Container>
     <Nav>
       <Hamburger/>
     </Nav> 
-  </>
+    <WhoAmI>
+      I’m Nishant Dania,<br />
+      a software developer
+      from India currently based in Singapore.
+    </WhoAmI>
+  </Container>
 )
 
 export default HomePage
