fc54e0e89b5a86e3c79f4c55986e9c5ff10d241a
nishantdania <nishantdania@gmail.com>
2020-07-22 12:19:46 +0800

    Updated nav

--- src/components/hero-image.js
+++ src/components/hero-image.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import styled from 'styled-components'
-
-const Image = styled(Img)`
-  bottom: 0;
-  left: 0;
-  right: auto;
-  top: 0;
-  width: 100%;
-`
 
 const HeroImage = ({ imagePath }) => {
   const data = useStaticQuery(graphql`
@@ -24,12 +15,7 @@ const HeroImage = ({ imagePath }) => {
     }
   `)
 
-  return (
-    <Image
-      fluid={data.placeholderImage.childImageSharp.fluid}
-      style={{ position: `fixed` }}
-    />
-  )
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
 export default HeroImage
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import '../styles/normalize.css'
 import '../styles/reset.css'
 import '../styles/global.css'
 import styled from 'styled-components'
+import Nav from './nav'
 
 const ContentContainer = styled.div`
   max-width: 700px;
@@ -10,6 +11,11 @@ const ContentContainer = styled.div`
   padding: 0 24px;
 `
 
-const Layout = ({ children }) => <ContentContainer>{children}</ContentContainer>
+const Layout = ({ children }) => (
+  <ContentContainer>
+    <Nav />
+    {children}
+  </ContentContainer>
+)
 
 export default Layout
