e48df07694484d7751b2628cbd129407f78926ff
nishantdania <nishantdania@gmail.com>
2020-08-06 18:38:05 +0800

    Test social image generation build time

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,10 +5,16 @@
  */
 
 // You can delete this file if you're not using it
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+
+const puppeteer = require('puppeteer')
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage, deletePage } = actions
 
   const blogPostTemplate = require.resolve(`./src/templates/post.js`)
+  const blogPostSocialImageTemplate = require.resolve(
+    `./src/templates/post-social-image.js`,
+  )
 
   return graphql(`
     {
@@ -30,7 +36,7 @@ exports.createPages = ({ actions, graphql }) => {
       return Promise.reject(result.errors)
     }
 
-    return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    return result.data.allMarkdownRemark.edges.forEach(async ({ node }) => {
       createPage({
         path: node.frontmatter.slug,
         component: blogPostTemplate,
@@ -39,6 +45,35 @@ exports.createPages = ({ actions, graphql }) => {
           slug: node.frontmatter.slug,
         },
       })
+
+      createPage({
+        path: `${node.frontmatter.slug}/seo`,
+        component: blogPostSocialImageTemplate,
+        context: {
+          // additional data can be passed via context
+          slug: node.frontmatter.slug,
+        },
+      })
+
+      const screenshot = async function (url, imagePath) {
+        const browser = await puppeteer.launch({ headless: false, timeout: 0 })
+        const page = await browser.newPage()
+        await page.goto(url)
+        await page.screenshot({ path: imagePath })
+        await browser.close()
+      }
+
+      let url = `http://localhost:8000${node.frontmatter.slug}/seo`
+      if (node.frontmatter.slug.includes('minimalist')) {
+        console.log('::::::::::::::::::HERE:::::::::::::::::::::')
+        console.log(url)
+        await screenshot(url, `src/social-images${node.frontmatter.slug}.png`)
+      }
+      // Create Screenshots for social images
+      // deletePage({
+      //   path: `${node.frontmatter.slug}/seo`,
+      //   component: blogPostSocialImageTemplate,
+      // })
     })
   })
 }
